0010d28b2c6a9fbe14bb52e7dbe76da18adbeaa7
baragon23 <baragon23@gmail.com>
2014-10-10 18:30:01 -0700

    'Alert Me' button now gets disabled when a user saves a movie

--- app/assets/javascripts/movieApp/movieCtrl.js
+++ app/assets/javascripts/movieApp/movieCtrl.js
@@ -1,5 +1,5 @@
 
-jQuery.getScript('youtube_api_call.js');
+//jQuery.getScript('youtube_api_call.js');
 
 movieApp.controller('MovieController', [ '$http', '$scope', 'Movie', function($http, $scope, Movie) {
 
@@ -9,6 +9,7 @@ movieApp.controller('MovieController', [ '$http', '$scope', 'Movie', function($h
 	$scope.movies = [ ];
 	var sorted_movie_array = [ ];
 
+	//call api and build sorted movie array to display on page
 	$http({ method: 'GET', url: apiURL })
 		.success(function(data) {
 			
@@ -33,13 +34,15 @@ movieApp.controller('MovieController', [ '$http', '$scope', 'Movie', function($h
 
 				//assign release date
 				movieObj.release_date = data.results[i].release_date;
+				
 				sorted_movie_array.push(movieObj);
 			}
 
+			 //sort by newest date
 			sorted_movie_array.sort(function(a, b){
 					var dateA = new Date(a.release_date);
 					var dateB = new Date(b.release_date);
-					return dateA - dateB; //sort by date ascending
+					return dateA - dateB;
 			});
 
 			$scope.movies = sorted_movie_array;
@@ -52,31 +55,34 @@ movieApp.controller('MovieController', [ '$http', '$scope', 'Movie', function($h
 	//===========================================
 
 	$scope.savedMovies = {};
-
 	$scope.newMovie = new Movie();
 
 	Movie.query(function(json) {
 		$scope.savedMovies = json;
 	});
-	//$scope.user_id = "";
-	$scope.addMovie = function(movie) {
+	
+	$scope.addMovie = function(movie, index) {
 		$scope.newMovie.title = movie.title;
 		$scope.newMovie.poster = movie.poster;
 		$scope.newMovie.release_date = movie.release_date;
+		//save the movie record along with the current user id so that way
+		//the records will be tied to that user through user id
 		$scope.newMovie.user_id = movie.user_id;
 
 		//now save the movie to the database
 		$scope.newMovie.$save(function() {
 			$scope.savedMovies.push($scope.newMovie);
 			$scope.newMovie = new Movie();
-			//reset the form
-			//$scope.postForm.$pristine = true;
+			
+			//deactivate the alert me button and change button text to say 'Saved'
+			jQuery('input[name="submit' + index + '"]').prop({
+				"disabled": true, 
+				"value": "Saved"
+			});
 		});
 	};
 
-	//movie was passed in from the html page in the destroyMovie function
 	$scope.destroyMovie = function(movie) {
-		console.log(movie);
 		movie.$delete(function() {
 			var position = $scope.savedMovies.indexOf(movie);
 			$scope.savedMovies.splice(position, 1);
