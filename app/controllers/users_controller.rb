4716a9a5f0d5657a85bc345492df46991315946e
baragon23 <baragon23@gmail.com>
2014-10-12 20:17:27 -0700

    used Zurb Foundation and custom styles to create a professional layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   before_action :check_user_logged_in, only: [:edit, :update, :destroy]
   before_action :set_user, only: [:edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
   def index
     if !current_user 
       redirect_to root_path
@@ -12,13 +10,10 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -27,44 +22,9 @@ class UsersController < ApplicationController
     else
       render :new
     end
-
-    # respond_to do |format|
-    #   if @user.save
-    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
-    #     format.json { render action: 'show', status: :created, location: @user }
-    #   else
-    #     format.html { render action: 'new' }
-    #     format.json { render json: @user.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   def edit
-
-  end
-
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-    end
   end
 
   private
