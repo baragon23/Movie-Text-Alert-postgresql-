f588b28550430aa906cd8e73d45b70d221fc595d
baragon23 <baragon23@gmail.com>
2014-10-15 20:56:18 -0700

    manually required angular files in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,9 @@
 //= require angular
 //= require angular-resource
 //= require angular-rails-templates
+//= require movieApp/movieApp
+//= require movieApp/movieService
+//= require movieApp/movieCtrl
 //= require_tree .
 
 $(document).foundation();
